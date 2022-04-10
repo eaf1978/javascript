a2d48709048e2b185ac1059d264b71924089ae49
eaf1978 <102886593+eaf1978@users.noreply.github.com>
2022-04-10 11:02:53 -0300

    práticas de programação aprendidas no dio
    práticas de programação aprendidas no dio

---
+++ introdução ao javascript/js/main.js
@@ -0,0 +1,24 @@
+var lista = ["maçã", "pera", "laranja"];
+//lista.push("uva");
+//lista.pop();
+console.log(lista[1]);
+//console.log(lista.length());
+//console.log(lista.reverse());
+alert(list[1]);
+
+
+
+
+
+
+//var nome = "Eunice de Faria";
+//var n1 = 44;
+//var n2 = 40;
+//var frase = "Hi World";
+//alert(nome + " tem " + n1 + " anos ");
+//alert(n1 + n2);
+//console.log(nome);
+//console.log(idade + idade2);
+//console.log(frase.touppercase());
+//console.log(frase.toLowercase());
+//alert(frase.replace("World", "there"));
